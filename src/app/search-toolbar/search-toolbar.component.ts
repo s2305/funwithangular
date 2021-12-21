f35401d6a95f3ddbc57b422935abc7f0fd88d329
Stephane MESTRE <stephane.mestre.p@citeo.com>
2021-12-21 16:51:22 +0100

    pour ajouter un autocomplete avec filtre

--- src/app/search-toolbar/search-toolbar.component.ts
+++ src/app/search-toolbar/search-toolbar.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {Observable, of} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-search-toolbar',
@@ -10,9 +13,18 @@ export class SearchToolbarComponent implements OnInit {
 
   myControl = new FormControl();
   options: string[] = ['One', 'Two', 'Three'];
- 
+  filteredOptions: Observable<string[]>=of(["yyy"]);
+
   ngOnInit() {
-  
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
   }
 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
