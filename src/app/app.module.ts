d7a1d7524be58897f0da30c88cb7909d9f54ec62
Stephane MESTRE <stephane.mestre.p@citeo.com>
2021-12-21 16:25:26 +0100

    pour intégrer material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,36 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import { SearchToolbarComponent } from './search-toolbar/search-toolbar.component';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    SearchToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    MatAutocompleteModule,
+    MatSliderModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
+HttpClientInMemoryWebApiModule.forRoot(
+  InMemoryDataService, { dataEncapsulation: false }
+),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,18 +12,14 @@ export class HeroesComponent implements OnInit {
 
   heroes:Hero[]=[];
 
-  selectedHero?: Hero;
-
-  constructor(private heroService:HeroService, private messageService: MessageService) { }
+  
+  constructor(private heroService:HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  
 
   getHeroes(): void {
      this.heroService.getHeroes().subscribe(results=>this.heroes=results);
